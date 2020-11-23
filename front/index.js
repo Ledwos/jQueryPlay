d252292475ec029fa6ce10aa8251c7e902869b0b
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-11-23 18:34:36 +0000

    more fxns

--- front/index.js
+++ front/index.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 
     // hide p tag
     $('button').click(() => {
-        $('#hiddenP').hide();
+        $('#hiddenP').hide('slow', function() {
+            alert("This occured after I was hidden, an example of using a callback");
+        });
     });
     // can select by Element / Id / Class
     $('p:nth-child(even)').dblclick(function() {
@@ -14,5 +16,15 @@ $(document).ready(function() {
     // hover alert p tag
     $('#hoverP').mouseenter(function() {
         alert("I warned you this would happen! Have a nice day :D");
+    });
+    // slide toggle
+    $('#slideMagic').click(function() {
+        $('#slideTrick').slideToggle('slow');
+    });
+    // animate? write CSS properties in camelCase 
+    // fetching text
+    $("#textCopier").click(function() {
+        let text = $('#textToCopy').text();
+        $('#textCopier').text('I\'d also' + text.slice(4,text.length));
     });
 });
