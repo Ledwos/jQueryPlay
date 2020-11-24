855f6033ea6ecf28cb667f2f909028248a198a2c
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-11-24 14:45:17 +0000

    basic form validation

--- front/index.js
+++ front/index.js
@@ -27,4 +27,22 @@ $(document).ready(function() {
         let text = $('#textToCopy').text();
         $('#textCopier').text('I\'d also' + text.slice(4,text.length));
     });
+
+    // form validation (the whole 'was-validated' not working as it should)
+    let frm = document.getElementById('basicForm');
+    // frm.addEventListener('submit', function(e) {
+    //     if (frm.checkValidity() === false) {
+    //         e.preventDefault();
+    //     }
+    //     frm.classList.add('was-validated');
+    //     e.preventDefault();
+    // });
+    $('#basicForm').submit(function(e) {
+        if (frm.checkValidity === true) {
+            $('form').addClass('was-validated');
+            e.preventDefault();
+        }
+        $('form').addClass('was-validated');
+        e.preventDefault();
+    })
 });
